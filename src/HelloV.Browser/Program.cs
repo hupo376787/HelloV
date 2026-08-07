011a5cdb2cbc190e6e8fb203df832a5c553dfda3
Vincent Wang <376787823@qq.com>
2026-08-07 20:48:56 +0800

    Persist interrupt mode in browser

--- src/HelloV.Browser/Program.cs
+++ src/HelloV.Browser/Program.cs
@@ -14,11 +14,19 @@ internal sealed partial class Program
     private static async Task Main(string[] args)
     {
         await JSHost.ImportAsync("HelloVBrowser", "../js/hellov-browser.js");
+        await JSHost.ImportAsync("HelloVSettings", "../js/hellov-settings.js");
 
         AppServices.PlatformKind = AppPlatformKind.Browser;
         AppServices.CameraFactory = static () => new BrowserCameraService();
         AppServices.EmojiImageProvider ??= new BrowserEmojiImageProvider();
         AppServices.ToggleFullscreenAsync = BrowserInterop.ToggleFullscreenAsync;
+        AppServices.LoadInterruptMode = static () => BrowserInterop.GetInterruptMode() switch
+        {
+            1 => true,
+            0 => false,
+            _ => null
+        };
+        AppServices.SaveInterruptMode = BrowserInterop.SetInterruptMode;
 
         await BuildAvaloniaApp()
             .WithFont_SourceHanSansCN()
