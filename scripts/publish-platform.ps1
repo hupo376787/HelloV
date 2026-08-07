d7e9f7adfe5d0073d0804ff7e958f6b60b824bca
Vincent Wang <376787823@qq.com>
2026-08-07 11:26:29 +0800

    Use xiaowei application identifier

--- scripts/publish-platform.ps1
+++ scripts/publish-platform.ps1
@@ -210,7 +210,7 @@ function New-MacAppBundle {
 <dict>
   <key>CFBundleName</key><string>HelloV</string>
   <key>CFBundleDisplayName</key><string>HelloV</string>
-  <key>CFBundleIdentifier</key><string>com.example.hellov</string>
+  <key>CFBundleIdentifier</key><string>com.xiaowei.hellov</string>
   <key>CFBundleExecutable</key><string>HelloV.Desktop</string>
   <key>CFBundlePackageType</key><string>APPL</string>
   <key>CFBundleIconFile</key><string>app-icon</string>
